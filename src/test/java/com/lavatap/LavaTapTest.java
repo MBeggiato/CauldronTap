ddb4e791c886adf61bf426141a7f648751157f26
MBeggiato <marcel@mbx.sh>
2026-05-22 11:48:51 +0200

    first commit

---
+++ src/test/java/com/lavatap/LavaTapTest.java
@@ -0,0 +1,12 @@
+package com.lavatap;
+
+import org.junit.jupiter.api.Test;
+
+/** Tests for {@link LavaTap}. */
+public class LavaTapTest {
+
+  @Test
+  public void placeholder() {
+    // Add tests when plugin logic is implemented.
+  }
+}
